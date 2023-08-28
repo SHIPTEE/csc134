0f8ff9b7bb9d39b9610a96da784c644239e115b5
SHIPTEE <mcmillat3950@student.faytechcc.edu>
2023-08-28 11:25:58 -0400

    Started M1Lab

---
+++ M1labMcMillan/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+CSC 134
+M1Lab - Apple Orchard
+mcmillant
+8/28
+Simple interactive store.
+*/
+
+
+int main()
+{
+    string name = "Tenikia";
+    cout << "Welcome to the " << name << " orchard." << endl;
+    return 0;
+}

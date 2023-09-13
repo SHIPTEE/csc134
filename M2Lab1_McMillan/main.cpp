3307578e402bbf1155ab727b5f52d9b16531acf5
SHIPTEE <143419938+SHIPTEE@users.noreply.github.com>
2023-09-13 11:07:49 -0400

    M2Lab1
    Initial setup

---
+++ M2Lab1_McMillan/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+
+/*
+M2Lab1 - Crate Example Case Study
+Find the volume of a crate of a given size
+Find the cost to make it
+Find the price we sell it for
+Find the profit
+
+*/
+
+using namespace std;
+
+int main()
+{
+    cout << "General Crates Pricing System" << endl;
+    // declare variables
+    const double COST_PER_CUBIC_FOOT = 0.23; // $ PER CUBIC FOOT
+    const double CHARGE_PER_CUBIC_FOOT = 0.5; // PER CUBIC FOOT
+
+    double length, width, height; // in feet
+    double volume; // in cubic feet
+    double cost, charge, profit; // $
+
+    // Get the crate dimensions
+    cout << "Enter length, width, and height in feet. Press ENTER after each value." << endl;
+    cin >> length >> width >> height;
+
+    //Calculate the volume
+    volume = length * width * height;
+
+    //Find the cost and price for that volume
+    //Find the profit
+
+    //Output the answer
+    cout << "The crate is: " << volume << " cubic ft." << endl;
+
+    return 0;
+}
